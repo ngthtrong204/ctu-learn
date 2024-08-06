7cb0e7356dedd020132fb95f4ce001500868f986
ngthtrong204 <ngthtrong204@gmail.com>
2024-08-06 22:00:15 +0700

    explain basic rule of language

---
+++ start/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] arg) {
+        boolean flag = true;
+        if (!flag) {
+            System.out.println(358 % 350);
+            System.out.println(50000);
+        } else {
+            System.out.println(Math.max(7, Math.max(5, Math.sqrt(81))));
+            System.out.println((int) (Math.random() * 100));
+        }
+    }
+
+}
