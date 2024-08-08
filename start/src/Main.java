19d88893422546cae019b2fdf7750bd34708ed04
ngthtrong204 <ngthtrong204@gmail.com>
2024-08-08 13:33:58 +0700

    type of method

---
+++ start/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  final int x = 10;
+  final double PI = 3.14;
+  public int age = 24;
+  
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
