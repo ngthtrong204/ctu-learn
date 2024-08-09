c316b2a45e101d7a8e1fb911d7343927c7e5a799
ngthtrong204 <ngthtrong204@gmail.com>
2024-08-09 21:56:00 +0700

    start Else java

---
+++ start/buoi1/Prime.java
@@ -0,0 +1,7 @@
+/**
+ * Prime
+ */
+public class Prime {
+
+  
+}
--- start/buoi1/sumTwoNumber.java
+++ start/buoi1/sumTwoNumber.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
+
 public class sumTwoNumber {
-  static void sum(int a, int b) {
-    System.out.println("Sum: " + (a + b));
+  static void sum() {
+    try (Scanner myScan = new Scanner(System.in)) {
+      int a = myScan.nextInt();
+      int b = myScan.nextInt();
+      System.out.println("Sum: " + (a + b));
+    }
   }
+
   public static void main(String[] args) {
-    Scanner myScan = new Scanner(System.in);
-    int a = myScan.nextInt();
-    int b = myScan.nextInt();
-    sum(a, b);
+    sum();
   }
 
 }
